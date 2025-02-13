9969606f357270569359da5ad6e2236343e18d7e
pi-mathiasdev <mathiaspereiradev@gmail.com>
2025-02-13 15:09:39 -0300

    Remove redundant import of global CSS in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { AuthProvider } from "@/context/AuthContext";
 import localFont from "next/font/local";
 import "./globals.css";
-import "@/styles/globals.css";
 
 const fontPrincipal = localFont({
   src: [
